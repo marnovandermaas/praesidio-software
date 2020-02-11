f6b0b93fb790eadaa78fad907bc5a38224305a58
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2020-02-11 18:14:23 +0000

    Rewrote driver test to test libc-based interface

--- benchmarks/driver/user.c
+++ benchmarks/driver/user.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-#include "praesidiouser.h"
+#include <fcntl.h>
+#include <sys/ioctl.h>
+//#include "praesidiouser.h"
 
 int main(void) {
-  char *tx_address = NULL;
+  char name[128]; //TODO include 128 from driver header file.
   printf("Starting driver test\n");
-  tx_address = NW_create_send_mailbox(1);
-  printf("Got address 0x%016lx\n", tx_address);
-  // tx_address[0] = 1;
-  // printf("Done.\n");
+  int file_descriptor = open("/dev/praesidio", O_RDWR);
+  if(file_descriptor == -1) {
+    printf("Failed to open main driver %s\n", name);
+    return -1;
+  }
+  ioctl(file_descriptor, 0, name);
+  printf("Got name %s\n", name);
 }
