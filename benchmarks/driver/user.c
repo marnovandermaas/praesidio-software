524929b8be9911e40c62d66f028f5d19693000b6
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2020-02-12 18:52:16 +0000

    Working driver benchmark with mmap call.

--- benchmarks/driver/user.c
+++ benchmarks/driver/user.c
@@ -4,7 +4,12 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/ioctl.h>
-//#include "praesidiouser.h"
+#include <sys/mman.h>
+#include "praesidiouser.h"
+
+//TODO get these defines from driver header.
+#define IOCTL_CREATE_ENCLAVE _IO('a', 1)
+#define IOCTL_CREATE_SEND_MAILBOX _IO('a', 2)
 
 int main(void) {
   char device_name[128]; //TODO include 128 from driver header file.
@@ -22,10 +27,24 @@ int main(void) {
   int enclave_descriptor = -1;
   printf("Opening enclave file %s", device_path);
   while(enclave_descriptor == -1) {
-    enclave_descriptor = open(name2, O_RDWR);
+    enclave_descriptor = open(device_path, O_RDWR);
     printf(".");
   }
   printf("\n");
-  ioctl(enclave_descriptor, 11, 13);
+
+  if (ioctl(enclave_descriptor, IOCTL_CREATE_ENCLAVE) < 0) { //create enclave: add enclave memory in here.
+    printf("Error: failed create enclave ioctl.\n");
+    return -1;
+  }
+  if (ioctl(enclave_descriptor, IOCTL_CREATE_SEND_MAILBOX) < 0) { //create send mailbox
+    printf("Error: failed to create send mailbox because %s.\n", strerror(errno));
+    return -1;
+  }
+  void *ptr = mmap(NULL, 1 << PAGE_BIT_SHIFT, PROT_WRITE, MAP_SHARED, enclave_descriptor, 0);
+  if(ptr != NULL) {
+    printf("Got following string from kernel page: %s\n", (char *) ptr);
+  } else {
+    printf("ERROR: Pointer is null.\n");
+  }
   return 0;
 }
