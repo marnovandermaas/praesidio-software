59694360014a60f25a5bf697683a1c1c241f4737
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2020-06-09 16:24:59 +0100

    Updated ring-buffer implementation to wrap. Changed ring benchmark to test this wrapping

--- benchmarks/ring/ring.h
+++ benchmarks/ring/ring.h
@@ -4,7 +4,7 @@
 #include "communication.h"
 
 #define START_PACKET_SIZE (2)
-#define NUMBER_OF_REPS (10)
-#define NUMBER_OF_SHIFTS (4)
+#define NUMBER_OF_REPS (4)
+#define NUMBER_OF_SHIFTS (10)
 
 #endif
--- lib/praesidio.c
+++ lib/praesidio.c
@@ -3,41 +3,70 @@
 
 //Returns the starting index for the next message.
 int send_enclave_message(char *mailbox_address, char *message, int length) {
+  int current_offset = (long long) mailbox_address & ((1 << PAGE_BIT_SHIFT) - 1);
   int offset = LENGTH_SIZE + length;
-  int i;
+  int i, index;
 
-  if(length > ((2 << PAGE_BIT_SHIFT)-2*LENGTH_SIZE)) {
-    return -1;
+  if(length > ((1 << PAGE_BIT_SHIFT)-2*LENGTH_SIZE)) {
+    return 0;
   }
 
-  mailbox_address[0] = BUSY_BYTE;
-
   for(i = 0; i < length; i++) {
-    mailbox_address[LENGTH_SIZE+i] = message[i];
+    index = LENGTH_SIZE + i;
+    if((current_offset + index) >= (1 << PAGE_BIT_SHIFT)) {
+      index -= (1 << PAGE_BIT_SHIFT);
+    }
+    mailbox_address[index] = message[i];
+  }
+
+  if((current_offset + offset) >= (1 << PAGE_BIT_SHIFT)) {
+    offset -= (1 << PAGE_BIT_SHIFT);
   }
 
   mailbox_address[offset] = BUSY_BYTE;
 
   for(i = 0; i < LENGTH_SIZE; i++) {
-    mailbox_address[LENGTH_SIZE-1-i] = ((length >> (8*i)) & 0xFF);
+    index = LENGTH_SIZE-1-i;
+    if((current_offset + index) >= (1 << PAGE_BIT_SHIFT)) {
+      index -= (1 << PAGE_BIT_SHIFT);
+    }
+    mailbox_address[index] = ((length >> (8*i)) & 0xFF);
   }// mailbox_address[1] = length & 0xFF; mailbox_address[0] = 0;
   return offset;
 }
 
 //Returns the starting index for the next message
-int get_enclave_message(volatile char *mailbox_address, char *read_buffer) {
-  int i;
+int get_enclave_message(volatile char *mailbox_address, char *read_buffer) {//TODO return the length of the received message as well
+  int current_offset = (long long) mailbox_address & ((1 << PAGE_BIT_SHIFT) - 1);
+  int i, index;
   int length = 0;
+  int ret_val = 0;
 
   while (mailbox_address[0] == BUSY_BYTE);
 
   for (i = 0; i < LENGTH_SIZE; i++) {
-    length |= (((int) (mailbox_address[i] & 0xFF)) << (8*(LENGTH_SIZE-1-i)));
+    index = i;
+    if((current_offset + index) >= (1 << PAGE_BIT_SHIFT)) {
+      index -= (1 << PAGE_BIT_SHIFT);
+    }
+    length |= (((int) (mailbox_address[index] & 0xFF)) << (8*(LENGTH_SIZE-1-i)));
   }// length = mailbox_address[1] & 0xFF;
 
-  for(i = 0; i < length; i++) {
-    read_buffer[i] = mailbox_address[LENGTH_SIZE+i];
+  if (length > ((1 << PAGE_BIT_SHIFT) - 2*LENGTH_SIZE)) {
+    return 0;
   }
 
-  return LENGTH_SIZE + length;
+  for(i = 0; i < length; i++) {
+    index = LENGTH_SIZE + i;
+    if((current_offset + index) >= (1 << PAGE_BIT_SHIFT)) {
+      index -= (1 << PAGE_BIT_SHIFT);
+    }
+    read_buffer[i] = mailbox_address[index];
+  }
+
+  ret_val = LENGTH_SIZE + length;
+  if((current_offset + ret_val) >= (1 << PAGE_BIT_SHIFT)) {
+    ret_val -= (1 << PAGE_BIT_SHIFT);
+  }
+  return ret_val;
 }
