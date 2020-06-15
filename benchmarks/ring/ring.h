b5cd3233e545b6ac3bfeca35a59be0dfd8660409
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2020-06-15 11:02:31 +0100

    Reduced packet max due to unusual results at large packet sizes.

--- benchmarks/ring/ring.h
+++ benchmarks/ring/ring.h
@@ -4,7 +4,7 @@
 #include "communication.h"
 #include "praesidiopage.h"
 
-#define PACKET_MAX        ((1 << (PAGE_BIT_SHIFT)) - 16*LENGTH_SIZE)
+#define PACKET_MAX        ((1 << (PAGE_BIT_SHIFT)) - 64*LENGTH_SIZE)
 #define PACKET_START      (1)
 #define PACKET_INCREMENT  (64)
 #define NUMBER_OF_REPS    (16)
