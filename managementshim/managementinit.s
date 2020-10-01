feade59982ed789e4d973d585a631f9b1299ddaa
Marno <git@techroose.com>
2020-10-01 14:19:27 +0100

    Some cleanup of managementinit.s

--- managementshim/managementinit.s
+++ managementshim/managementinit.s
@@ -7,8 +7,8 @@ entry:
   la    t1, __stack
   la    t2, __offset
   add   t1, t0, t1
-  csrrs t0, 0xF14, zero
-  mul   t2, t2, t0 #Calculate stack offset for this core's management code.
+  csrr  t0, mhartid # Get hart id
+  mul   t2, t2, t0 # Calculate stack offset for this core's management code
   add   sp, t1, t2
   beq   t0, zero, normal
 
@@ -17,12 +17,12 @@ enclave:
   csrw  mtvec, t0
   call  initialize
   jalr  x1, a0, 0
-  j entry
+  j     entry
 
 normal:
   call  normalWorld
   la    t0, __dram
-  add   sp, x0, x0 #Load zero into sp.
+  add   sp, x0, x0 # Load zero into sp
   jalr  x0, t0, 0
 
 loop:
